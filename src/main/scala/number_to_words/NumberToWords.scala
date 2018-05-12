8bf588e44de38823cc14d16db45c300c50732a85
Dorren Chen <dorrenchen@gmail.com>
2018-05-12 00:32:06 -0400

    done number to words, for now

--- src/main/scala/number_to_words/NumberToWords.scala
+++ src/main/scala/number_to_words/NumberToWords.scala
@@ -1,83 +1,62 @@
 package number_to_words
 
 object NumberToWords {
-  val convMap =
+  val convMap:Map[Long, String] =
     Map(
-       0 -> "zero",
-       1 -> "one",       2 -> "two",       3 -> "three",
-       4 -> "four",      5 -> "five",      6 -> "six",
-       7 -> "seven",     8 -> "eight",     9 -> "nine",
-      10 -> "ten",      11 -> "eleven",   12 -> "twelve",
-      13 -> "thirteen", 14 -> "fourteen", 15 -> "fifteen",
-      16 -> "sixteen",  17 -> "seventeen",18 -> "eighteen",
-      19 -> "nineteen",
-      20 -> "twenty",   30 -> "thirty",   40 -> "forty",
-      50 -> "fifty",    60 -> "sixty",    70 -> "seventy",
-      80 -> "eighty",   90 -> "ninety",
-      100 -> "hundred",
-      1000 -> "thousand",
-      1000000 -> "million",
-      1000000000 -> "billion")
+       0L -> "zero",
+       1L -> "one",       2L -> "two",       3L -> "three",
+       4L -> "four",      5L -> "five",      6L -> "six",
+       7L -> "seven",     8L -> "eight",     9L -> "nine",
+      10L -> "ten",      11L -> "eleven",   12L -> "twelve",
+      13L -> "thirteen", 14L -> "fourteen", 15L -> "fifteen",
+      16L -> "sixteen",  17L -> "seventeen",18L -> "eighteen",
+      19L -> "nineteen",
+      20L -> "twenty",   30L -> "thirty",   40L -> "forty",
+      50L -> "fifty",    60L -> "sixty",    70L -> "seventy",
+      80L -> "eighty",   90L -> "ninety",
+      100L -> "hundred",
+      1000L -> "thousand",
+      1000000L -> "million",
+      1000000000L -> "billion")
 
   /**
     * convert a number to English word
     */
-  def convert(num: Int, partial:Boolean = false): String = {
+  def convert(num: Long, partial:Boolean = false): String = {
     num match {
-      case 0 if partial     => null
-      case x if x < 20      => convMap(num)
-      case x if x < 100     => do2digits(x)
-      case x if x < 1000    => do3digits(x)
-      case x if x < 1000000 => do5digits(x)
+      case 0 if partial            => null
+      case x if x < 20L            => convMap(num)
+      case x if x < 100L           => do2digits(x)
+      case x if x < 1000L          => do3digits(x)
+      case x if x < 1000000L       => do6digits(x)
+      case x if x < 1000000000L    => do9digits(x)
+      case x if x < 1000000000000L => do12digits(x)
     }
   }
 
-  def do2digits(num: Int, partial: Boolean = false): String = {
-    if(num < 10){
-      convert(num, partial)
-    }else {
-      val remainder = num % 10 // last digit.
-      val quotient_num = num - remainder // with last digit = 0
-
-      Seq(convMap(quotient_num), convert(remainder, true))
-        .filter(_ != null)
-        .mkString(" ")
-    }
+  def do2digits(num: Long, partial: Boolean = false): String = {
+    process(num, 10, convert, partial)
   }
 
-  def do3digits(num: Int, partial: Boolean = false): String = {
-    if(num < 100){
-      do2digits(num, partial)
-    }else {
-      val remainder = num % 100 // last 2 digit
-      val quotient_num = num - remainder // with last 2 digits as 0
-
-      val quotient = quotient_num / 100
-      val quotient_word = convMap(quotient) + " " + convMap(100)
-
-      Seq(quotient_word, do2digits(remainder, true))
-        .filter(_ != null)
-        .mkString(" ")
-    }
+  def do3digits(num: Long, partial: Boolean = false): String = {
+    process(num, 100, do2digits, partial)
   }
 
-  def do5digits(num: Int, partial: Boolean = false): String = {
-    if(num < 1000){
-      do3digits(num, partial)
-    }else {
-      val remainder = num % 1000 // last 2 digit
-      val quotient_num = num - remainder // with last 3 digits as 0
-
-      val quotient = quotient_num / 1000
-      val quotient_word = convert(quotient) + " " + convMap(1000)
-
-      Seq(quotient_word, do3digits(remainder, true))
-        .filter(_ != null)
-        .mkString(" ")
-    }
+  def do6digits(num: Long, partial: Boolean = false): String = {
+    process(num, 1000, do3digits, partial)
   }
 
-  def process(num: Int, keyNum: Int, nextFn: (Int, Boolean) => String, partial: Boolean = false): String = {
+  def do9digits(num: Long, partial: Boolean = false): String = {
+    process(num, 1000000, do6digits, partial)
+  }
+
+  def do12digits(num: Long, partial: Boolean = false): String = {
+    process(num, 1000000000, do9digits, partial)
+  }
+
+  private def process(num: Long, keyNum: Long,
+                      nextFn: (Long, Boolean) => String,
+                      partial: Boolean = false): String = {
     if(num < keyNum){
       nextFn(num, partial)
     }else {
@@ -85,7 +64,10 @@ object NumberToWords {
       val quotient_num = num - remainder
 
       val quotient = quotient_num / keyNum
-      val quotient_word = convert(quotient) + " " + convMap(keyNum)
+      val quotient_word = if(keyNum < 100)
+                            convMap(quotient_num)
+                          else
+                            convert(quotient, true) + " " + convMap(keyNum)
 
       Seq(quotient_word, nextFn(remainder, true))
         .filter(_ != null)
--- src/test/scala/number_to_words/NumberToWordsSpec.scala
+++ src/test/scala/number_to_words/NumberToWordsSpec.scala
@@ -4,29 +4,41 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class NumberToWordsSpec extends FlatSpec with Matchers{
   "it" should "convert single digit" in {
-    assert("zero" == NumberToWords.convert(0))
-    assert("one"  == NumberToWords.convert(1))
-    assert("nine" == NumberToWords.convert(9))
+    assert(NumberToWords.convert(0) == "zero")
+    assert(NumberToWords.convert(1) == "one")
+    assert(NumberToWords.convert(9) == "nine")
   }
 
   "it" should "convert teens" in {
-    assert("eleven"  == NumberToWords.convert(11))
-    assert("nineteen" == NumberToWords.convert(19))
+    assert(NumberToWords.convert(11) == "eleven")
+    assert(NumberToWords.convert(19) == "nineteen")
   }
 
   "it" should "convert 2 digits" in {
-    assert("twenty"  == NumberToWords.convert(20))
-    assert("thirty one" == NumberToWords.convert(31))
+    assert(NumberToWords.convert(20) == "twenty")
+    assert(NumberToWords.convert(31) == "thirty one")
   }
 
   "it" should "convert 3 digits" in {
-    assert("one hundred"  == NumberToWords.convert(100))
-    assert("two hundred thirty four" == NumberToWords.convert(234))
+    assert(NumberToWords.convert(100) == "one hundred")
+    assert(NumberToWords.convert(234) == "two hundred thirty four")
   }
 
-  "it" should "convert 5 digits" in {
-    assert("one thousand"  == NumberToWords.convert(1000))
-    assert("two thousand three" == NumberToWords.convert(2003))
-    assert("thirty four thousand five hundred sixty seven" == NumberToWords.convert(34567))
+  "it" should "convert 4-6 digits" in {
+    assert(NumberToWords.convert(1000)   == "one thousand")
+    assert(NumberToWords.convert(2003)   == "two thousand three")
+    assert(NumberToWords.convert(34567)  == "thirty four thousand five hundred sixty seven")
+    assert(NumberToWords.convert(300000) == "three hundred thousand")
+    assert(NumberToWords.convert(304000) == "three hundred four thousand")
+  }
+
+  "it" should "convert 6-9 digits" in {
+    assert(NumberToWords.convert(1000000) == "one million")
+    assert(NumberToWords.convert(2300000) == "two million three hundred thousand")
+  }
+
+  "it" should "convert up to 10-12 digits" in {
+    assert(NumberToWords.convert(1000000000) == "one billion")
+    assert(NumberToWords.convert(1002000000) == "one billion two million")
   }
 }
